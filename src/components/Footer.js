3170c229dd3211d922ebbbb8b4961b405e4b05c7
johndamilola <johndamilola03@gmail.com>
2016-08-16 05:55:47 +0100

    first commit

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Footer extends Component {
-  render() {
-    return (
-      <h1>I am a footer</h1>
-    );
-  }
-}
--- src/components/Header/Title.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Footer extends Component {
-  render() {
-    return (
-      <h1>This is a Title</h1>
-    );
-  }
-}
